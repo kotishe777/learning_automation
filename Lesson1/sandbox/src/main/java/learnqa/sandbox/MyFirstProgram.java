bd95cb975ee796bd09279af98d3bd4bed65d4a68
kotishe777 <65562279+kotishe777@users.noreply.github.com>
2020-06-09 23:43:50 +0300

    Some functions are added

--- Lesson1/sandbox/src/main/java/learnqa/sandbox/MyFirstProgram.java
+++ Lesson1/sandbox/src/main/java/learnqa/sandbox/MyFirstProgram.java
@@ -2,8 +2,25 @@ package learnqa.sandbox;
 
 public class MyFirstProgram{
 
-    public static void main(String[] args){
-        System.out.println("Hello World");
+  public static void main(String[] args){
+    hello("world");
+    hello("user");
+    hello("Alexei");
 
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 }
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+  public static double area(double l){
+    return l * l;
+  }
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
